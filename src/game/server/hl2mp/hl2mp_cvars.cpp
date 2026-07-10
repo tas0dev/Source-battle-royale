2e53caf63c04a5efebdaee18b8bf27eba33691c2
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-10 15:19:23 +0900

    Init

--- src/game/server/hl2mp/hl2mp_cvars.cpp
+++ src/game/server/hl2mp/hl2mp_cvars.cpp
@@ -8,18 +8,30 @@
 #include "cbase.h"
 #include "hl2mp_cvars.h"
 
-
-
 // Ready restart
 ConVar mp_readyrestart(
-							"mp_readyrestart", 
-							"0", 
-							FCVAR_GAMEDLL,
-							"If non-zero, game will restart once each player gives the ready signal" );
+    "mp_readyrestart",
+    "0",
+    FCVAR_GAMEDLL,
+    "If non-zero, game will restart once each player gives the ready signal"
+);
 
 // Ready signal
 ConVar mp_ready_signal(
-							"mp_ready_signal",
-							"ready",
-							FCVAR_GAMEDLL,
-							"Text that each player must speak for the match to begin" );
+    "mp_ready_signal",
+    "ready",
+    FCVAR_GAMEDLL,
+    "Text that each player must speak for the match to begin"
+);
+
+// Round duration
+ConVar bp_round_time(
+    "bp_round_time",
+    "180",
+    FCVAR_GAMEDLL | FCVAR_NOTIFY,
+    "Round duration in seconds",
+    true,
+    30.0f,
+    true,
+    1200.0f
+);
